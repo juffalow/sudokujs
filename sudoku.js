f960e79b73db6aa3a33d560a133ae9252cdf8b96
Matej Jellus <juffalow@juffalow.com>
2017-08-31 13:14:16 +0200

    private members fix

--- sudoku.js
+++ sudoku.js
@@ -1,40 +1,4 @@
 /**
- *
- */
-var sudoku = [];
-/**
- *
- */
-var emptyCells = [];
-/**
- *
- */
-var rowRule = [];
-/**
- *
- */
-var colRule = [];
-/**
- *
- */
-var squareRule = [];
-/**
- *
- *
- * @param  {number} row
- * @param  {number} col
- */
-var onCellChange = function(row, col) {};
-/**
- *
- *
- * @param  {number} row
- * @param  {number} col
- * @param  {number} value
- */
-var onValueChange = function(row, col, value) {};
-
-/**
  * Checks whether array is valid sudoku array, i.e. 9x9.
  *
  * @param  {array} arr
@@ -57,104 +21,6 @@ function checkBoard(board) {
     }
 }
 
-
-/**
- *
- */
-function prepare() {
-    for( var i = 0; i < 9; i++ ) {
-        rowRule[i] = [ false, false, false, false, false, false, false, false, false ];
-        colRule[i] = [ false, false, false, false, false, false, false, false, false ];
-        squareRule[i] = [ false, false, false, false, false, false, false, false, false ];
-        sudoku[i] = [ null, null, null, null, null, null, null, null, null ];
-    }
-}
-
-function analyze(input) {
-    for( var i = 0; i < 9; i++ ) {
-        for( var j = 0; j < 9; j++ ) {
-            if( !set(i, j, input[i][j]) ) {
-                throw "Sudoku is not valid!";
-            }
-            if( input[i][j] == null ) {
-                emptyCells.push({
-                    row: i,
-                    col: j
-                });
-            }
-        }
-    }
-}
-
-function backtrack(index) {
-    if( index >= emptyCells.length ) {
-        return true;
-    }
-
-    onCellChange(emptyCells[index].row, emptyCells[index].col);
-
-    for( var i = 1; i <= 9; i++ ) {
-        if( set(emptyCells[index].row, emptyCells[index].col, i) ) {
-            onValueChange(emptyCells[index].row, emptyCells[index].col, i);
-            if( backtrack(index + 1) ) {
-                return true;
-            }
-        }
-    }
-    set(emptyCells[index].row, emptyCells[index].col, null);
-    return false;
-}
-
-
-/**
- *
- *
- * @param  {number} row
- * @param  {number} col
- * @param  {number} value
- * @return {boolean}
- */
-function set(row, col, value) {
-    if( value !== null && !canSetValue(row, col, value) ) {
-        return false;
-    }
-
-    updateRules(row, col, sudoku[row][col], false);
-    updateRules(row, col, value, true);
-    sudoku[row][col] = value;
-
-    return true;
-}
-
-/**
- *
- *
- * @param  {number} row
- * @param  {number} col
- * @param  {number} value
- * @param  {boolean} state
- */
-function updateRules(row, col, value, state) {
-    if( value !== null ) {
-        rowRule[row][value - 1] = state;
-        colRule[col][value - 1] = state;
-        squareRule[parseInt(row / 3) + (parseInt(col / 3) * 3)][value - 1] = state;
-    }
-}
-
-/**
- *
- *
- * @param  {number} row
- * @param  {number} col
- * @param  {number} value
- * @return {boolean}
- */
-function canSetValue(row, col, value) {
-    return !(rowRule[row][value - 1] || colRule[col][value - 1] || squareRule[parseInt(row / 3) + (parseInt(col / 3) * 3)][value - 1]);
-}
-
-
 /**
  *
  * @author Matej 'juffalow' Jellus <juffalow@juffalow.com>
@@ -166,31 +32,210 @@ export default class Sudoku {
      *
      *
      * @param  {array} board
-     * @param  {function} onCellChangeListener
-     * @param  {function} onValueChangeListener
      * @return {object}
      */
-    constructor(board, onCellChangeListener, onValueChangeListener) {
-        checkBoard(board);
+    constructor(board) {
+        /**
+         *
+         */
+        this._sudoku = [];
+        /**
+         *
+         */
+        this._emptyCells = [];
+        /**
+         *
+         */
+        this._rowRule = [];
+        /**
+         *
+         */
+        this._colRule = [];
+        /**
+         *
+         */
+        this._squareRule = [];
+        /**
+         *
+         */
+        this._sleep = 0;
+        /**
+         *
+         *
+         * @param  {number} row
+         * @param  {number} col
+         */
+        var _onCellChange = function(row, col) {};
+        /**
+         *
+         *
+         * @param  {number} row
+         * @param  {number} col
+         * @param  {number} value
+         */
+        var _onValueChange = function(row, col, value) {};
+        /**
+         *
+         *
+         * @param  {array} result
+         */
+        var _onFinish = function(result) {};
 
+        checkBoard(board);
+        this._prepare();
+        this._analyze(board);
+    }
+
+    solve() {
+        this._backtrack(0);
+        return this._sudoku;
+    }
+
+    /**
+     *
+     *
+     * @param  {function} onCellChangeListener
+     * @param  {function} onValueChangeListener
+     * @param  {function} onFinishListener
+     * @param  {number} delay
+     */
+    solveWithDelay(onCellChangeListener, onValueChangeListener, onFinishListener, delay) {
         if( typeof onCellChangeListener === "function" ) {
-            onCellChange = onCellChangeListener;
-        } else if( typeof onCellChangeListener !== "undefined" ) {
+            this._onCellChange = onCellChangeListener;
+        } else {
             throw "Argument onCellChangeListener must be function!";
         }
 
         if( typeof onValueChangeListener === "function" ) {
-            onValueChange = onValueChangeListener;
-        } else if( typeof onValueChangeListener !== "undefined" ) {
+            this._onValueChange = onValueChangeListener;
+        } else {
             throw "Argument onValueChangeListener must be function!";
         }
 
-        prepare();
-        analyze(board);
+        if( typeof onFinishListener === "function" ) {
+            this._onFinish = onFinishListener;
+        } else {
+            throw "Argument onFinishListener must be function!";
+        }
+
+        this._sleep = delay || 0;
+
+        this._nonRecursiveBacktrack(0);
     }
 
-    solve() {
-        backtrack(0);
-        return sudoku;
+
+    /**
+     *
+     */
+    _prepare() {
+        for( var i = 0; i < 9; i++ ) {
+            this._rowRule[i] = [ false, false, false, false, false, false, false, false, false ];
+            this._colRule[i] = [ false, false, false, false, false, false, false, false, false ];
+            this._squareRule[i] = [ false, false, false, false, false, false, false, false, false ];
+            this._sudoku[i] = [ null, null, null, null, null, null, null, null, null ];
+        }
+    }
+
+    _analyze(input) {
+        for( var i = 0; i < 9; i++ ) {
+            for( var j = 0; j < 9; j++ ) {
+                if( !this._set(i, j, input[i][j]) ) {
+                    throw "Sudoku is not valid!";
+                }
+                if( input[i][j] === null ) {
+                    this._emptyCells.push({
+                        row: i,
+                        col: j
+                    });
+                }
+            }
+        }
+    }
+
+    _backtrack(index) {
+        if( index >= this._emptyCells.length ) {
+            return true;
+        }
+
+        for( var i = 1; i <= 9; i++ ) {
+            if( this._set(this._emptyCells[index].row, this._emptyCells[index].col, i) ) {
+                if( this._backtrack(index + 1) ) {
+                    return true;
+                }
+            }
+        }
+        this._set(this._emptyCells[index].row, this._emptyCells[index].col, null);
+        return false;
+    }
+
+    _nonRecursiveBacktrack(step) {
+        if( step >= this._emptyCells.length ) {
+            this._onFinish(this._sudoku);
+            return;
+        }
+
+        setTimeout(function() {
+            this._onCellChange(this._emptyCells[step].row, this._emptyCells[step].col);
+            for( var i = 0 + this._sudoku[this._emptyCells[step].row][this._emptyCells[step].col] + 1; i <= 9; i++ ) {
+                if( this._set(this._emptyCells[step].row, this._emptyCells[step].col, i) ) {
+                    this._onValueChange(this._emptyCells[step].row, this._emptyCells[step].col, i);
+                    this._nonRecursiveBacktrack(step + 1);
+                    return;
+                }
+            }
+            if( step > 0 ) {
+                this._set(this._emptyCells[step].row, this._emptyCells[step].col, null);
+                this._onValueChange(this._emptyCells[step].row, this._emptyCells[step].col, null);
+                this._nonRecursiveBacktrack(step - 1);
+            }
+        }.bind(this), this._sleep);
+    }
+
+    /**
+     *
+     *
+     * @param  {number} row
+     * @param  {number} col
+     * @param  {number} value
+     * @return {boolean}
+     */
+    _set(row, col, value) {
+        if( value !== null && !this._canSetValue(row, col, value) ) {
+            return false;
+        }
+
+        this._updateRules(row, col, this._sudoku[row][col], false);
+        this._updateRules(row, col, value, true);
+        this._sudoku[row][col] = value;
+
+        return true;
+    }
+
+    /**
+     *
+     *
+     * @param  {number} row
+     * @param  {number} col
+     * @param  {number} value
+     * @param  {boolean} state
+     */
+    _updateRules(row, col, value, state) {
+        if( value !== null ) {
+            this._rowRule[row][value - 1] = state;
+            this._colRule[col][value - 1] = state;
+            this._squareRule[parseInt(row / 3) + (parseInt(col / 3) * 3)][value - 1] = state;
+        }
+    }
+
+    /**
+     *
+     *
+     * @param  {number} row
+     * @param  {number} col
+     * @param  {number} value
+     * @return {boolean}
+     */
+    _canSetValue(row, col, value) {
+        return !(this._rowRule[row][value - 1] || this._colRule[col][value - 1] || this._squareRule[parseInt(row / 3) + (parseInt(col / 3) * 3)][value - 1]);
     }
 }
--- test/test.js
+++ test/test.js
@@ -33,38 +33,6 @@ describe('test sudoku', function() {
         }).to.throw("Sudoku is not valid!");
     });
 
-    it('should throw error if onCellChangeListener argument is not a function', () => {
-        expect(function() {
-            var sudoku = new Sudoku([
-                [ null, null, null, null, null, null, 6, 8, null ],
-                [ null, null, null, null, 7, 3, null, null, 9 ],
-                [ 3, null, 9, null, null, null, null, 4, 5 ],
-                [ 4, 9, null, null, null, null, null, null, null ],
-                [ 8, null, 3, null, 5, null, 9, null, 2 ],
-                [ null, null, null, null, null, null, null, 3, 6 ],
-                [ 9, 6, null, null, null, null, 3, null, 8 ],
-                [ 7, null, null, 6, 8, null, null, null, null ],
-                [ null, 2, 8, null, null, null, null, null, null ],
-            ], "something");
-        }).to.throw("Argument onCellChangeListener must be function!");
-    });
-
-    it('should throw error if onValueChangeListener argument is not a function', () => {
-        expect(function() {
-            var sudoku = new Sudoku([
-                [ null, null, null, null, null, null, 6, 8, null ],
-                [ null, null, null, null, 7, 3, null, null, 9 ],
-                [ 3, null, 9, null, null, null, null, 4, 5 ],
-                [ 4, 9, null, null, null, null, null, null, null ],
-                [ 8, null, 3, null, 5, null, 9, null, 2 ],
-                [ null, null, null, null, null, null, null, 3, 6 ],
-                [ 9, 6, null, null, null, null, 3, null, 8 ],
-                [ 7, null, null, 6, 8, null, null, null, null ],
-                [ null, 2, 8, null, null, null, null, null, null ],
-            ], function() {}, "something");
-        }).to.throw("Argument onValueChangeListener must be function!");
-    });
-
     it('should solve the sudoku', () => {
         var sudoku = new Sudoku([
             [ null, null, null, null, null, null, 6, 8, null ],
@@ -89,5 +57,85 @@ describe('test sudoku', function() {
             [ 7, 3, 1, 6, 8, 2, 5, 9, 4 ],
             [ 5, 2, 8, 9, 3, 4, 1, 6, 7 ]
         ]);
+    });
+
+    it('should throw error if onCellChangeListener argument is not a function', () => {
+        expect(function() {
+            var sudoku = new Sudoku([
+                [ null, null, null, null, null, null, 6, 8, null ],
+                [ null, null, null, null, 7, 3, null, null, 9 ],
+                [ 3, null, 9, null, null, null, null, 4, 5 ],
+                [ 4, 9, null, null, null, null, null, null, null ],
+                [ 8, null, 3, null, 5, null, 9, null, 2 ],
+                [ null, null, null, null, null, null, null, 3, 6 ],
+                [ 9, 6, null, null, null, null, 3, null, 8 ],
+                [ 7, null, null, 6, 8, null, null, null, null ],
+                [ null, 2, 8, null, null, null, null, null, null ],
+            ]);
+            sudoku.solveWithDelay("something");
+        }).to.throw("Argument onCellChangeListener must be function!");
+    });
+
+    it('should throw error if onValueChangeListener argument is not a function', () => {
+        expect(function() {
+            var sudoku = new Sudoku([
+                [ null, null, null, null, null, null, 6, 8, null ],
+                [ null, null, null, null, 7, 3, null, null, 9 ],
+                [ 3, null, 9, null, null, null, null, 4, 5 ],
+                [ 4, 9, null, null, null, null, null, null, null ],
+                [ 8, null, 3, null, 5, null, 9, null, 2 ],
+                [ null, null, null, null, null, null, null, 3, 6 ],
+                [ 9, 6, null, null, null, null, 3, null, 8 ],
+                [ 7, null, null, 6, 8, null, null, null, null ],
+                [ null, 2, 8, null, null, null, null, null, null ],
+            ]);
+            sudoku.solveWithDelay(function() {}, "something");
+        }).to.throw("Argument onValueChangeListener must be function!");
+    });
+
+    it('should throw error if onFinishListener argument is not a function', () => {
+        expect(function() {
+            var sudoku = new Sudoku([
+                [ null, null, null, null, null, null, 6, 8, null ],
+                [ null, null, null, null, 7, 3, null, null, 9 ],
+                [ 3, null, 9, null, null, null, null, 4, 5 ],
+                [ 4, 9, null, null, null, null, null, null, null ],
+                [ 8, null, 3, null, 5, null, 9, null, 2 ],
+                [ null, null, null, null, null, null, null, 3, 6 ],
+                [ 9, 6, null, null, null, null, 3, null, 8 ],
+                [ 7, null, null, 6, 8, null, null, null, null ],
+                [ null, 2, 8, null, null, null, null, null, null ],
+            ]);
+            sudoku.solveWithDelay(function() {}, function() {}, "something");
+        }).to.throw("Argument onFinishListener must be function!");
+    });
+
+    it('should solve with delay', function(done) {
+        this.timeout(60000);
+        var sudoku = new Sudoku([
+            [ 1, 7, 2, 5, 4, 9, 6, 8, 3 ],
+            [ 6, 4, 5, 8, 7, 3, 2, 1, 9 ],
+            [ 3, 8, 9, 2, 6, 1, 7, 4, 5 ],
+            [ 4, 9, 6, 3, 2, 7, 8, 5, 1 ],
+            [ 8, 1, 3, 4, 5, 6, 9, 7, 2 ],
+            [ 2, 5, 7, 1, 9, 8, 4, 3, 6 ],
+            [ 9, 6, 4, 7, 1, 5, 3, 2, 8 ],
+            [ 7, 3, 1, 6, 8, 2, 5, 9, 4 ],
+            [ null, 2, 8, null, null, null, null, null, null ],
+        ]);
+        sudoku.solveWithDelay(function() {}, function() {}, function(result) {
+            expect(result).to.deep.equal([
+                [ 1, 7, 2, 5, 4, 9, 6, 8, 3 ],
+                [ 6, 4, 5, 8, 7, 3, 2, 1, 9 ],
+                [ 3, 8, 9, 2, 6, 1, 7, 4, 5 ],
+                [ 4, 9, 6, 3, 2, 7, 8, 5, 1 ],
+                [ 8, 1, 3, 4, 5, 6, 9, 7, 2 ],
+                [ 2, 5, 7, 1, 9, 8, 4, 3, 6 ],
+                [ 9, 6, 4, 7, 1, 5, 3, 2, 8 ],
+                [ 7, 3, 1, 6, 8, 2, 5, 9, 4 ],
+                [ 5, 2, 8, 9, 3, 4, 1, 6, 7 ]
+            ]);
+            done();
+        }, 1);
     });
 });
